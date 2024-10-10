d3449b319f3e0a61e052533f962841716a1a9446
TRIUYEN_TANG <triuyentang@gmail.com>
2024-10-10 14:02:36 +0200

    "re_arrange guessing game"

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number !");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    // let appl = 2  //  not mutable
+    // let mut appl = 2 // is mutalble
+    // associated function : a function implemented on a type
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+        let guess: u32 = guess.trim()
+                                .parse()
+                                .expect("Please type a number!");
+                                
+    println!("You guessed {guess}");
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+    // varialbes are immutable by default
+    // reference a immutable by default.
+    // you must write : &mut guess
+
+}
