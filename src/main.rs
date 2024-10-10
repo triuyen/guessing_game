10d619e5407c8378f2fdab8ba4bbe01bbf6cd15a
TRIUYEN_TANG <triuyentang@gmail.com>
2024-10-10 16:35:53 +0200

    [FINISH] game

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number !");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
     
     loop {
         println!("Please input your guess.");
@@ -17,28 +16,23 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
         
-        println!("Please input guess : ");
-        
         let guess: u32 = match guess.trim()
                                 .parse() {
                                     Ok(num) => num,
                                     Err(_) => continue,
-                                        };
+        };
                                
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number){
             Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too Big!"),
+            Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
     }
-    // varialbes are immutable by default
-    // reference a immutable by default.
-    // you must write : &mut guess
 }
 
 
