40975800cf5e9a1481fe49178ea41c3b0c4dac61
TRIUYEN_TANG <triuyentang@gmail.com>
2024-10-10 13:50:24 +0200

    " re-arrange files and folder "

--- lib.rs
+++
@@ -1,6 +0,0 @@
-#[no_mangle]
-pub fn add(left : u64, right: u64) -> u64{ // unsign long u64
-    left + right
-}
-
-    
--- main.rs
+++
@@ -1,5 +0,0 @@
-fn main(){
-    println!("Hello world!");
-    // wait for 3 seconds
-    std::thread::sleep(std::time::Duration::from_secs(3));
-}
